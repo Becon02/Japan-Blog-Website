88dbf9392b6e0068e10c8d459af2bad0d6761cf4
Martin Sanz Sanchez <martinuco569@gmail.com>
2025-04-21 12:07:39 +0100

    Add gallery grid styles, update journal entries with timestamps, and enhance TypeScript configuration

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,8 @@ import { z, defineCollection } from "astro:content";
 const journalCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    date: z.date(), // "YYYY-MM-DD"
+    date: z.coerce.date(), // Automatically converts ISO string to Date
+    timestamp: z.coerce.date(), // Same here
   }),
 });
 
